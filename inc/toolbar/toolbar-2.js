4ccceca0a20305d8c17b0d564ecfebc16ef7517e
BassMatiOnCode <bassmatiOnMusic@freenet.de>
2021-08-20 20:43:26 +0200

    Toolbar buttons and creator function improved.

--- inc/toolbar/toolbar-2.js
+++ inc/toolbar/toolbar-2.js
@@ -1,5 +1,5 @@
 //
-// toolbar-2.js
+// toolbar-2.js  2021-08-20  usp
 //
 
 let toolbar ;
@@ -17,11 +17,11 @@ export function createButtons( navigation, collapsible ) {
 	/// Parameters provide the necessary methods that are used by the buttons.
 	let button;
 	createButton( "svg", "navigate", "svg", steeringWheel, navigation.navigateButtonClickHandler ); 
-	createButton( "a", "navigate-up", "svg", upArrow, navigation.parent );
-	createButton( "a", "navigate-first", "svg", leftEndArrow, navigation.first );
-	createButton( "a", "navigate-previous", "svg", leftArrow, navigation.previous );
-	createButton( "a", "navigate-next", "svg", rightArrow, navigation.next );
-	createButton( "a", "navigate-last", "svg", rightEndArrow, navigation.last );
+	createButton( "a", "navigate-up", "svg", circledArrowUp, navigation.parent );
+	createButton( "a", "navigate-first", "svg", circledArrowFirst, navigation.first );
+	createButton( "a", "navigate-previous", "svg", circledArrowLeft, navigation.previous );
+	createButton( "a", "navigate-next", "svg", circledArrowRight, navigation.next );
+	createButton( "a", "navigate-last", "svg", circledArrowLast, navigation.last );
 	if ( document.querySelector( "body > #content > [cbc]" )) {
 		button = createButton( "svg", "collapse-toggle", "svg", circledPlusMinus, collapsible.toggleAllBlocks );
 		button.setAttribute( "expand", "" );
@@ -38,8 +38,8 @@ function createButton( tag, name, imgtype, imgurl, option ) {
 	let button;
 	tag = tag.toUpperCase( );
 	// Create the button element
-	button = tag === "SVG" ? createSVG( imgurl ) : document.createElement( tag );
-	button.setAttribute( "class", "toolbutton" );
+	button = tag === "SVG" ? createSVGElement( imgurl ) : document.createElement( tag );
+	button.setAttribute( "class", "tool button" );
 	button.setAttribute( "name", name ) ;
 	// A buttons have an href element, the others have an event handler attached.
 	if ( tag === "A" ) button.setAttribute( "href", option );
@@ -50,7 +50,7 @@ function createButton( tag, name, imgtype, imgurl, option ) {
 	case "DIV" : 
 		switch ( imgtype ) {
 		case "url" : button.appendChild( createImage( imgurl )); break;
-		case "svg" : button.appendChild( createSVG( imgurl )); break;
+		case "svg" : button.appendChild( createSVGElement( imgurl )); break;
 		case "obj" : button.appendChild( imgurl ); break;
 		} }
 	toolbar.insertBefore( button, null );
@@ -63,34 +63,30 @@ function createImage( url ) {
 	return image;
 	}
 
-function createSVG( content ) {
-	const e = document.createElementNS( "http://www.w3.org/2000/svg", "svg" );
-	e.setAttribute( "xmlns:xlink", "http://www.w3.org/1999/xlink" );
+function createSVGElement( drawingInstructions ) {
+	const svgNameSpace = "http://www.w3.org/2000/svg" ;
+	const e = document.createElementNS( svgNameSpace, "svg" );
+	e.setAttribute( "version", "1.1" );
+	e.setAttribute( "class", "tool button" );
 	e.setAttribute( "viewBox", "-50 -50 100 100" );
-	// Make sure width and height matches attributes in toolbar.css .toolbutton, plus 1!
 	e.setAttribute( "width", "33" );
 	e.setAttribute( "height", "33" );
-	e.setAttribute( "version", "1.1" );
-	e.setAttribute( "class", "svg-toole" );
-	e.innerHTML = content ;
+	const g = document.createElementNS( svgNameSpace, "g" );
+	g.setAttribute( "stroke", "darkorange" );
+	g.setAttribute( "stroke-width", "6" );
+	g.setAttribute( "stroke-linecap", "round" );
+	g.setAttribute( "stroke-linejoin", "round" );
+	g.setAttribute( "fill", "white" );
+	e.appendChild( g );
+	g.innerHTML = drawingInstructions;
 	return e ;
-	/*
-	var svgimg = document.createElementNS( "http://www.w3.org/2000/svg","image");
-	svgimg.setAttribute("height","100");
-	svgimg.setAttribute("width","100");
-	svgimg.setAttribute("id","testimg2");
-	svgimg.setAttributeNS("http://www.w3.org/1999/xlink","href","some/imgae.jpg");
-	svgimg.setAttribute("x","0");
-	svgimg.setAttribute("y","0");
-	button.appendChild(svgimg);
-	*/
 	}
 
 	// Toolbar icons SVG code
-const steeringWheel = "<g stroke-width='7'><ellipse cx='0' cy='0' rx='37' ry='37'' /><ellipse cx='0' cy='0' rx='12' ry='12' /><path d='M 0, -46 L 0, 46 M -46, 0 L 46, 0 M -32.53, -32.53 L 32.53, 32.53 M 32.53, -32.53 L -32.53, 32.53' />" ;
-const upArrow = "<path d='M 0, -35 L 0, 35 M -35, 0 L 0, -35 L 35, 0' />" ;
-const leftEndArrow = "<path d='M -35, 0 L 35, 0 M -35, 40 L -35, -40 M 10, 35 L -28, 0 L 10, -35' />" ;
-const leftArrow =  "<path d='M -35, 0 L 35, 0 M 0, 35 L -35, 0 L 0, -35' />" ;
-const rightArrow = "<path d='M -35, 0 L 35, 0 M 0, 35 L 35, 0 L 0, -35' />" ;
-const rightEndArrow = "<path d='M -35, 0 L 35, 0 M 35, -40 L 35, 40 M -10, 35 L 28, 0 L -10, -35' />" ;
-const circledPlusMinus = "<ellipse cx='0' cy='0' rx='45' ry='45' /><path d='M -25, 0 L 25, 0' /><path class='expand' d='M 0, -25 L 0, 25' />" ;
+const steeringWheel = '<circle cx="0" cy="0" r="38" /><circle cx="0" cy="0" r="22"/><path d="M 0, -46 L 0, 46 M -46, 0 L 46, 0 M -32.53, -32.53 L 32.53, 32.53 M 32.53, -32.53 L -32.53, 32.53"/>' ;
+const circledArrowUp = '<circle cx="0" cy="0" r="46"/><path d="M 0 25 L 0 -25 M -22 -4 L 0 -25 22 -4"/>' ;
+const circledArrowFirst = '<circle cx="0" cy="0" r="46"/><path d="M -22 0 L 25 0 M 0 -22 L -21 0 0 22 M -25 -20 L -25 20"/>' ;
+const circledArrowLeft = '<circle cx="0" cy="0" r="46"/><path d="M -25 0 L 25 0 M -4 -22 L -25 0 -4 22"/>' ;
+const circledArrowRight = '<circle cx="0" cy="0" r="46"/><path d="M -25 0 L 25 0 M 4 -22 L 25 0 4 22"/>' ;
+const circledArrowLast = '<circle cx="0" cy="0" r="46"/><path d="M -25 0 L 22 0 M 0 -22 L 21 0 0 22 M 25 -20 L 25 20"/>' ;
+const circledPlusMinus = '<circle cx="0" cy="0" r="46"/><path d="M -25 0 L 25 0"/><path id="vertical" d="M 0 -25 L 0 25"/>' ;
