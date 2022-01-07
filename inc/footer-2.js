fe522210c5a92fee17d662d7a7af01f3f86c6e98
BassMatiOnCode <ulrich.sprick@gmx.de>
2022-01-07 13:47:11 +0100

    Changed footer content generation for sites without navigation TOC

--- inc/footer-2.js
+++ inc/footer-2.js
@@ -1,4 +1,4 @@
-//
+﻿//
 // footer-2.js  2021-08-03  usp
 //
 
@@ -15,20 +15,27 @@ export function initPage ( ) {
 	const pageCreator = getMetaData( "author" );
 	const changeDate = getMetaData( "change-date" );
 	const pageEditor = getMetaData( "editor" );
-	const pageVersion = getMetaData( "version", "0" );
-	let footer = document.createElement( "DIV" );
+	const pageVersion = getMetaData("version", "0");
+	let footer = document.getElementById( "page-footer" );
+	if ( ! footer ) { 
+		const footers = document.getElementsByTagName( "FOOTER" );
+		if ( footers ) footer = footers[ footers.length - 1 ];
+		}
+	if ( ! footer ) document.createElement( "DIV" );
 	footer.id = "page-footer" ;
-	footer.innerHTML = `Created: ${creationDate} by ${pageCreator} &bull;  Edited: ${changeDate} by ${pageEditor} &bull; Version: ${pageVersion}<br/><a href=\"/index.htm\">Home</a> | ` ;
+	footer.innerHTML = `Created ${creationDate} by ${pageCreator} &bull;  Edited ${changeDate} by ${pageEditor} &bull; Version8 ${pageVersion}<br/><a href=\"/index.htm\">Home</a> | ` ;
 	document.body.appendChild( footer );
 	}
 
-export function addLinks( navigation ) {
+export function addNavigationLinks( navigation ) {
 	let s = "" ;
-	if ( navigation.parent !== "#" ) s += `<a href="${navigation.parent}">Up</a> | `
-	if ( navigation.first !== "#" ) s += `<a href="${navigation.first}">First</a> | `
-	if ( navigation.previous !== "#" ) s += `<a href="${navigation.previous}">Previous</a> | `
-	if ( navigation.next !== "#" ) s += `<a href="${navigation.next}">Next</a> | `
-	if ( navigation.last !== "#" ) s += `<a href="${navigation.last}">Last</a> | `
+	if ( navigation ) {
+		if ( navigation.parent !== "#" ) s += `<a href="${navigation.parent}">Up</a> | `
+		if ( navigation.first !== "#" ) s += `<a href="${navigation.first}">First</a> | `
+		if ( navigation.previous !== "#" ) s += `<a href="${navigation.previous}">Previous</a> | `
+		if ( navigation.next !== "#" ) s += `<a href="${navigation.next}">Next</a> | `
+		if ( navigation.last !== "#" ) s += `<a href="${navigation.last}">Last</a> | `
+		}
 	s += '<a href="/legal.htm">Legal</a>' ;
 	document.getElementById( "page-footer" ).innerHTML += s ;
 	}
