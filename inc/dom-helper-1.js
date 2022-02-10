5a2b0e62ad03628472459aff3065041c6b320d76
BassMatiOnCode <bassmatiOnMusic@freenet.de>
2022-02-10 09:09:41 +0100

    files moved to the docs folder

--- inc/dom-helper-1.js
+++
@@ -1,9 +0,0 @@
-﻿///
-///  dom-helper-1.js   ES2016 module   2021-10-22   usp
-///  
-
-export function swapAttributes( e, a, b ) {
-	const s = e.getAttribute( a );
-	e.setAttribute( a, e.getAttribute( b ));
-	e.setAttribute( b, s );
-	}
