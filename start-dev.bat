a2f40d84b396eff190db93bb9306bc6a9072808a
BassMatiOnCode <bassmatiOnMusic@freenet.de>
2022-02-13 15:20:16 +0100

    .

--- start-dev.bat
+++ start-dev.bat
@@ -1,4 +1,4 @@
-d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\site-root"
+d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\bassmati-on-code\site-root"
+cd /d d:\github\bassmati-on-code\site-root\docs
 start /min deno-file-server
 start "msedge.exe" "http://localhost:4507/index.htm"
-
