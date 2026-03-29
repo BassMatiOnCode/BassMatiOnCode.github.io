9151dcb1d6e9d0b8d96c0ecbc7e012f5b3d50e94
BassMati <75748577+BassMatiOnCode@users.noreply.github.com>
2026-03-29 11:31:13 +0200

    intermediate update

---
+++ docs/spa-iframe-content.js
@@ -0,0 +1,3 @@
+import "/inc/webcat/spa/spa-iframe-content.js" ;
+import "/inc/webcat/collapsible-structures/collapsible-structures-1.js?init-event-name=fragment-loading-complete" ;
+import "/inc/webcat/content-height-observer/content-height-observer.js" ;
